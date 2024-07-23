f6d37b23015f13fde39c69f52641267846d52e9b
darksidePardo <jjpardo2002@gmail.com>
2024-07-23 08:50:14 -0500

    feat: NA

--- src/main/java/com/apperror/app_error/controllers/AppController.java
+++ src/main/java/com/apperror/app_error/controllers/AppController.java
@@ -1,10 +1,6 @@
 package com.apperror.app_error.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/app")
 public class AppController {
 
 }
