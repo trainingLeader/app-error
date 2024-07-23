9bdc74d8f2d4ae21cff1f15a1714982bd91014c7
darksidePardo <jjpardo2002@gmail.com>
2024-07-22 22:54:46 -0500

    feat: :sparkles: Add data Appconfig
    Add data users

---
+++ src/main/java/com/apperror/app_error/AppConfig.java
@@ -0,0 +1,24 @@
+package com.apperror.app_error;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.apperror.app_error.domain.entity.User;
+
+@Configuration
+public class AppConfig {
+ 
+    @Bean
+    List<User> users() {
+        List<User> users = new ArrayList<>();
+        users.add(new User(1L, "Pepe", "Gonzalez"));
+        users.add(new User(2L, "Andres", "Mena"));
+        users.add(new User(3L, "Maria", "Perez"));
+        users.add(new User(4L, "Josefa", "Ramirez"));
+        users.add(new User(5L, "Ale", "Gutierrez"));
+        return users;
+    }
+}
--- src/main/java/com/apperror/app_error/controllers/AppController.java
+++ src/main/java/com/apperror/app_error/controllers/AppController.java
@@ -2,9 +2,12 @@ package com.apperror.app_error.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apperror.app_error.domain.entity.User;
+import com.apperror.app_error.exceptions.UserNotFoundException;
 import com.apperror.app_error.service.UserService;
 
 @RestController
@@ -19,5 +22,11 @@ public class AppController {
         int value = Integer.parseInt("20x");
         System.out.println(value);
         return "ok 200";
+    }
+    @GetMapping("/show/{id}")
+    public User show(@PathVariable(name = "id") Long id) {
+        User user = service.findById(id).orElseThrow(() -> new UserNotFoundException("Error el usuario no existe!"));
+        System.out.println(user.getLastname());
+        return user;
     }
 }
