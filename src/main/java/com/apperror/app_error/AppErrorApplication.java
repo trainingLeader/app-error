8fdf42ef67635bf40a22977c92381da5d762079a
darksidePardo <jjpardo2002@gmail.com>
2024-07-22 22:14:37 -0500

    feat: :tada: Start project
    Initialized project

---
+++ src/main/java/com/apperror/app_error/AppErrorApplication.java
@@ -0,0 +1,13 @@
+package com.apperror.app_error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppErrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apperror/app_error/AppErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apperror.app_error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
